a6f58d6369e284adde7196c5b513c31b6580f0a1
jungsom <junyk0414@naver.com>
2024-10-24 15:52:57 +0900

    Initial commit

---
+++ hereforus/src/main/java/com/swproject/hereforus/HereforusApplication.java
@@ -0,0 +1,13 @@
+package com.swproject.hereforus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HereforusApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HereforusApplication.class, args);
+	}
+
+}
---
+++ hereforus/src/test/java/com/swproject/hereforus/HereforusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.swproject.hereforus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HereforusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
